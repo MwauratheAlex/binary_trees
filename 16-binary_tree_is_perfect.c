679e311a10edaeaaa85d295f69c48c7683f724a3
MwauratheAlex <mwaurathealex@gmail.com>
2023-03-10 11:21:01 +0300

    add task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdio.h>
 /**
  * max - returns the greater of two integer values
  *
@@ -25,7 +26,7 @@ int height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right  == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 	return (max(height(tree->left), height(tree->right) + 1));
 }
@@ -43,5 +44,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (tree->right == NULL && tree->left == NULL)
 		return (1);
-	return (height(tree->left) == height(tree->right));
+
+	return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right) &&
+		height(tree->left) == height(tree->right));
 }
